a84345456c29a0c851b6c2c41ee63031ceedbcdd
Viatcheslav Lebedev <lvv12001@gmail.com>
2022-06-10 17:39:07 +0300

    Initial Commit

---
+++ OpenWeatherMap/OpenWeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherMapApp.swift
+//  OpenWeatherMap
+//
+//  Created by Viatcheslav Lebedev on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
